e1d34465025a4c60da8548ad5f3d8ec4417a4d6b
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-07-08 14:32:48 +0545

    connected to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,13 @@ import cors from 'cors';
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI as string)
+.then(() => {
+    console.log("connected to Mongodb")
+}).catch((err) => {
+    console.log("unable to connect to Mongodb" + err.message);
+})
+
 app.use(cors());
 
 app.listen('7000', ()=> {
