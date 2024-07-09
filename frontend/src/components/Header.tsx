36194c050b12c884a997dbeba49b3a5ea8856f7b
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-07-09 10:43:58 +0545

    setup desktop nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -14,6 +15,9 @@ const Header = () => {
         </Link>
         <div className="md:hidden">
           <MobileNav/>
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="text-xl font-bold hover:text-orange-500 hover:bg-white"
+    >
+      Login
+    </Button>
+  );
+};
+
+export default MainNav;
